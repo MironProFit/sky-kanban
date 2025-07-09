5a0628837a15e041be7eecb3589358e6d9488eaa
MironProFit <mpf_3d@vk.com>
2025-07-10 00:11:44 +0500

    добавление функционаламодального окна

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import Main from './components/Main/Main'
 import Header from './components/Header/Header'
-import PopExit from './components/PopExit/PopExit'
-import PopBrowse from './components/PopBrowse/PopBrowse'
-import PopNewCard from './components/PopNewCard/PopNewCard'
-import Calendar from './components/Calendar/Calendar'
-import Card from './components/Card/Card'
-import Column from './components/Column/Column'
+// import PopExit from './components/PopExit/PopExit'
+// import PopBrowse from './components/PopBrowse/PopBrowse'
+// import PopNewCard from './components/PopNewCard/PopNewCard'
+// import Calendar from './components/Calendar/Calendar'
+// import Card from './components/Card/Card'
+// import Column from './components/Column/Column'
 import './App.css'
 
 function App() {
